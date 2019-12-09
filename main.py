3d5918e4432ed7c324d53a2b48f6a0fc5e3ccc64
Nicky Logan <laurentiusdominick@yahoo.com>
2019-12-09 19:17:29 +0700

    Change global variable

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 from jumble import Jumble
 
+ngram = 4
+filename = "wordlist/words_eng.txt"
+
 def main():
-    filename = "words_eng.txt"
     jumble = (
         Jumble.Builder()
-            .set_source_file(filename)
-            .set_ngram(4)
-            .build()
+        .set_source_file(filename)
+        .set_ngram(ngram)
+        .build()
     )
-    
+
     # keep generating words
     while True:
         print(jumble.generate_word(), end="")
         input()
 
+
 if __name__ == "__main__":
     main()
-
-
